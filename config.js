733e265972e892e398e06f62a962d1f46a656fa1
Kiragamiki <104133775+Kiragamiki@users.noreply.github.com>
2022-05-21 13:40:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,21 @@
 const CONFIG = {
-    introTitle: 'Chi à!',
+    introTitle: 'Nghi à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi này :))
+    Lạc vào tim anh lẻ loi này <33
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'phải chăng anh đã yêu từ cái nhìn đầu tiên',
+    btnIntro: '<333',
+    title: 'Tấc đất như 1 tấc vàng
+Đôi môi ngọt ngào như bầu trời đêm Suốt bao năm tháng cấy cày
+Cuối cùng nàng cũng lọt vào tim ta',
     desc: 'làm người yêu anh nhá :> ',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh à :3',
+    btnNo: 'Không, Anh :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà :). Yêu em nhiều nhiều :)))',
+    mess: 'Anh biết mà Yêu em nhiều nhiều <333',
     messDesc: 'yêu công chúa.',
     btnAccept: 'Okiiiii lun <333',
-    messLink: 'https://www.facebook.com/profile.php?id=100054771392926' //link mess của các bạn https://www.messenger.com/t/100002861780475/
+    messLink: 'https://www.facebook.com/DethSouth.Shizid/' //link mess của các bạn https://www.facebook.com/messages/t/100077278943063
 }
